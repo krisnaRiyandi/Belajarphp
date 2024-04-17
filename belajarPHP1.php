f9cf7b7a087af6ad08ea7866244fb0077427d78c
krisnaRiyandi <krisnariyandi_2226240155@mhs.mdp.ac.id>
2024-04-17 18:55:31 +0700

    belajarphp1
    belajar

---
+++ belajarPHP1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Script php dalam html</h1>
+    <!-- echo menampilkan kaliamt -->
+    <?php
+        echo "You and I <br>";
+    
+        $data1="data pertama";
+        $data2='data kedua';
+        $data3=100;
+        $data4=20.5;
+
+        echo $data1;
+        echo "<br>";
+        echo $data2." - ".$data3;
+        $hasil = $data3*$data4;
+        echo "<br>";
+        echo $hasil;
+        echo "<br>";
+        if($data4>=30 && $data4<=50){
+            echo "Range data 30-50";
+        }else{
+            echo "Di luar range";
+        }
+            
+    ?>
+</body>
+</html>
---
+++ belajarPHP2.php
@@ -0,0 +1,8 @@
+<?php
+    $b1= $_GET["nilai1"];
+    $b2= $_GET["nilai2"];
+
+    echo $b1;
+    echo "<br>";
+    echo $b2;
+?>
